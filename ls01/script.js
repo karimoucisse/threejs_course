0340b1f30263f39514419fa89a8d85081299b939
karimou cisse <karimou.cisse1@gmail.com>
2025-01-21 17:37:51 +0100

    add first threejs canvas with a red cube

--- ls01/script.js
+++ ls01/script.js
@@ -1,1 +1,18 @@
-console.log('Hello Karimou cisse');
+import * as THREE from 'three';
+const canvas = document.querySelector('canvas.webgl')
+const sizes = {
+	width: 800,
+	heigth: 600,
+	z : 3
+}
+const scene = new THREE.Scene()
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 'red'})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.heigth)
+camera.position.z = sizes.z
+scene.add(camera)
+const render = new THREE.WebGLRenderer({canvas})
+render.setSize(sizes.width, sizes.heigth)
+render.render(scene, camera)
